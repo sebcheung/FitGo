be2cba9265b7bf107b4babf0344ca2df145612ae
Sebastian Cheung <sebcheung64@gmail.com>
2025-04-13 18:28:51 -0400

    removed useless code

--- app/src/pages/01_Workout_Log.py
+++ app/src/pages/01_Workout_Log.py
@@ -3,7 +3,6 @@ logger = logging.getLogger(__name__)
 import pandas as pd
 import streamlit as st
 from streamlit_extras.app_logo import add_logo
-import world_bank_data as wb
 import matplotlib.pyplot as plt
 import numpy as np
 import plotly.express as px
@@ -18,12 +17,6 @@ st.header('World Bank Data')
 # You can access the session state to make a more customized/personalized app experience
 st.write(f"### Hi, {st.session_state['first_name']}.")
 
-# get the countries from the world bank data
-with st.echo(code_location='above'):
-    countries:pd.DataFrame = wb.get_countries()
-   
-    st.dataframe(countries)
-
 # the with statment shows the code for this block above it 
 with st.echo(code_location='above'):
     arr = np.random.normal(1, 1, size=100)
@@ -31,11 +24,3 @@ with st.echo(code_location='above'):
     ax.hist(arr, bins=20)
 
     st.pyplot(test_plot)
-
-
-with st.echo(code_location='above'):
-    slim_countries = countries[countries['incomeLevel'] != 'Aggregates']
-    data_crosstab = pd.crosstab(slim_countries['region'], 
-                                slim_countries['incomeLevel'],  
-                                margins = False) 
-    st.table(data_crosstab)
