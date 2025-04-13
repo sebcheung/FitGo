2eb37af74eb68fb1e08a710046b45e45105b7cb4
Sebastian Cheung <sebcheung64@gmail.com>
2025-04-13 16:25:31 -0400

    minor change in configuring db name

--- api/backend/rest_entry.py
+++ api/backend/rest_entry.py
@@ -31,7 +31,7 @@ def create_app():
     app.config['MYSQL_DATABASE_PASSWORD'] = os.getenv('MYSQL_ROOT_PASSWORD').strip()
     app.config['MYSQL_DATABASE_HOST'] = os.getenv('DB_HOST').strip()
     app.config['MYSQL_DATABASE_PORT'] = int(os.getenv('DB_PORT').strip())
-    app.config['MYSQL_DATABASE_DB'] = os.getenv('Fitgo').strip()  # Change this to your DB name
+    app.config['MYSQL_DATABASE_DB'] = 'Fitgo'
 
     # Initialize the database object with the settings above. 
     app.logger.info('current_app(): starting the database connection')
