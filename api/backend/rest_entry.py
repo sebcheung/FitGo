5933ced99185949961bbb978eee5af3b5467993f
Sebastian Cheung <sebcheung64@gmail.com>
2025-04-13 23:17:06 -0400

    replaced localhost with db

--- api/backend/rest_entry.py
+++ api/backend/rest_entry.py
@@ -29,7 +29,7 @@ def create_app():
     # app.config['MYSQL_DATABASE_USER'] = 'root'
     app.config['MYSQL_DATABASE_USER'] = 'root'
     app.config['MYSQL_DATABASE_PASSWORD'] = 'password'
-    app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+    app.config['MYSQL_DATABASE_HOST'] = 'db'
     app.config['MYSQL_DATABASE_PORT'] = 3306
     app.config['MYSQL_DATABASE_DB'] = 'Fitgo'
 
